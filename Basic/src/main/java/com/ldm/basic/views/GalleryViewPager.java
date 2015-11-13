4b1cb59dc443caf50821a6736e6db9288139a372
lidaming <lidaming@syswin.com>
2015-11-13 16:44:27 +0800

    修改GalleryViewPagerBUG

--- Basic/src/main/java/com/ldm/basic/views/GalleryViewPager.java
+++ Basic/src/main/java/com/ldm/basic/views/GalleryViewPager.java
@@ -9,7 +9,7 @@ import com.nineoldandroids.view.ViewHelper;
 
 /**
  * Created by ldm on 15/11/12.
- * 画册
+ * 画廊，这里默认使用了Gallery3DPageTransformer动画控制器
  */
 public class GalleryViewPager extends LViewPager {
 
@@ -24,21 +24,44 @@ public class GalleryViewPager extends LViewPager {
         init();
     }
 
-
+    /**
+     * 初始化，这里默认设置了一个Gallery3DPageTransformer动画控制器，
+     * 如果需要自定义动画可以调用setPageTransformer(...)
+     */
     public void init() {
-        this.setPageTransformer(true, new DepthPageTransformer());
+        this.setPageTransformer(true, new Gallery3DPageTransformer());
     }
 
-    public class DepthPageTransformer implements ViewPager.PageTransformer {
+    /**
+     * 动画控制器，这里做了一个3D相册的动画
+     */
+    public class Gallery3DPageTransformer implements ViewPager.PageTransformer {
+
         /**
          * 这个值用来控制倾斜角度
          */
         final float ROTATION_OFF_VALUE = -30f;
 
         public void transformPage(View view, float position) {
+
+            /**
+             * 计算缩放
+             */
             ViewHelper.setRotationY(view, position * ROTATION_OFF_VALUE);
-            final float tran = position * ROTATION_OFF_VALUE;
+
+            /**
+             * 计算位移距离
+             */
+            final float tran = position * (ROTATION_OFF_VALUE) * 1.45f;
             ViewHelper.setTranslationX(view, tran);
+
+            /**
+             * 计算缩放比例
+             */
+            final float s = Math.max(0.75f, 0.75f + (1 - Math.abs(position)) * 0.25f);
+            ViewHelper.setScaleX(view, s);
+            ViewHelper.setScaleY(view, s);
+
         }
     }
 
